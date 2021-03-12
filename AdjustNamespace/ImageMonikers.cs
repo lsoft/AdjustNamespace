65863fcb4275b0470e9688e324e222724543e1b4
lsoft <lsoft@list.ru>
2021-03-12 14:16:58 +0500

    * fixes related with publication

--- AdjustNamespace/ImageMonikers.cs
+++ AdjustNamespace/ImageMonikers.cs
@@ -15,7 +15,7 @@ namespace AdjustNamespace
         } = new ImageMoniker
         {
             Guid = new Guid("872022f4-493a-4d7b-97f5-8b474662c341"),
-            Id = 2
+            Id = 0
         };
 
 
