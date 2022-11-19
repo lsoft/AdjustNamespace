c57c72d2ac06236890c8b4059a43bc18952513a3
lsoft <lsoft@list.ru>
2022-11-19 12:05:11 +0500

    norefs Additional XAML tests.

---
+++ Tests/Standard/TestProject/Exp5/UserControl1.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SubjectWrong.Exp5
+{
+    /// <summary>
+    /// Interaction logic for UserControl1.xaml
+    /// </summary>
+    public partial class UserControl1 : UserControl
+    {
+        public static string StaticString = "do not delete";
+
+        public UserControl1()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Tests/Standard/TestProject/Exp5/UserControl6.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TestProject.Exp5
+{
+    /// <summary>
+    /// Interaction logic for UserControl6.xaml
+    /// </summary>
+    public partial class UserControl6 : UserControl
+    {
+        public UserControl6()
+        {
+            InitializeComponent();
+        }
+    }
+}
