a4d18bb65a211d6aa400617cb8f5bd29d8cdec98
lsoft <lsoft@list.ru>
2022-11-28 20:33:47 +0500

    norefs switched to 0.2.0; minor changes

--- AdjustNamespace.VsixShared/Adjusting/Fixer/FixerSet.cs
+++ AdjustNamespace.VsixShared/Adjusting/Fixer/FixerSet.cs
@@ -1,18 +1,8 @@
-﻿using AdjustNamespace.Adjusting.Fixer;
-using AdjustNamespace.Adjusting.Fixer.Specific;
-using AdjustNamespace.Helper;
-using Microsoft.VisualStudio.Shell.Interop;
-using Microsoft.VisualStudio.Shell;
-using Microsoft.VisualStudio.TextManager.Interop;
+﻿using AdjustNamespace.Adjusting.Fixer.Specific;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Controls;
-using Microsoft.VisualStudio.Text.Editor;
-using Microsoft.VisualStudio;
-using Microsoft.VisualStudio.Text;
+using Task = System.Threading.Tasks.Task;
 
 namespace AdjustNamespace.Adjusting.Fixer
 {
